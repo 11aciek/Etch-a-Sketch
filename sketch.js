638549fada489198df6649c148a17b2e90b716b0
11aciek <10337645+11aciek@users.noreply.github.com>
2024-11-01 14:38:48 +0100

    Add randomColor and Progressive Darkening

--- sketch.js
+++ sketch.js
@@ -3,12 +3,16 @@ const HEIGHT = 900;
 const container = document.querySelector(".container");
 const newGrid = document.querySelector(".new-grid");
 const showGrid = document.querySelector(".show-grid");
+const randColor = document.querySelector(".color");
+const darkeningColor = document.querySelector(".darkening");
 
 container.style.width = `${WIDTH}px`;
 container.style.height = `${HEIGHT}px`;
 
 let isSketching = false;
 let cellNumber = 16;
+let color = "rgb(67 67 243)";
+let isDarkening = false;
 
 container.addEventListener("mousedown", () => isSketching = true);
 container.addEventListener("mouseup", () => isSketching = false);
@@ -27,10 +31,8 @@ newGrid.addEventListener("click", function() {
 
   function clearBoard() {
 	let cells = document.querySelectorAll(".cell");
-	console.log(cells);
 	cells.forEach(c => container.removeChild(c));
 	cells = null;
-	console.log(cells);
   }
 }); 
 
@@ -41,18 +43,44 @@ function makeGrid(cellNumber) {
 	cell.style.height = `${HEIGHT/cellNumber}px`;
 	cell.classList.add("cell");
 	container.appendChild(cell);
-	cell.addEventListener("mousemove", function() {
+
+	cell.addEventListener("mouseover", function() {
 	  if (isSketching) {
-	  cell.style["background-color"] = "blue";
+		if (cell.classList.contains("non-blank") && isDarkening) {
+		  cell.style.opacity = Number(cell.style.opacity) + 0.10;
+		  cell.style["background-color"] = color;
+		} else {
+		cell.style["background-color"] = color;
+		cell.classList.add("non-blank");
+		cell.style.opacity = isDarkening ? "0.5" : "1.0";
+		}
 	  }
 	});
   }
+
   showGrid.classList.remove("active");
 }
 
 showGrid.addEventListener("click", function() {
   container.querySelectorAll(".cell").forEach(c => c.classList.toggle("grid"));
   showGrid.classList.toggle("active");
+});
+
+randColor.addEventListener("click", function() {
+  color = changeColor();
+  randColor.style["background-color"] = color;
+  container.querySelectorAll(".cell").forEach(c => c.classList.remove("non-blank"));
+  
+  function changeColor() {
+	const rgb = [0,0,0].map(() => Math.floor(Math.random() * 255));
+	return `rgb(${rgb[0]} ${rgb[1]} ${rgb[2]})`;
+  }
+});
+
+darkeningColor.addEventListener("click", function() {
+  isDarkening = isDarkening ? false : true;
+  darkeningColor.classList.toggle("active");
+  container.querySelectorAll(".cell").forEach(c => c.classList.remove("non-blank"));
 });
 
 makeGrid(cellNumber);
