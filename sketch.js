b7888c14d5a907e4133378f37acb1620be29efe2
11aciek <10337645+11aciek@users.noreply.github.com>
2024-11-01 12:58:05 +0100

    Add buttons newGRid and showGrid

--- sketch.js
+++ sketch.js
@@ -1,25 +1,58 @@
 const WIDTH = 900;
 const HEIGHT = 900;
-const CELL_NUMBER = 100;
 const container = document.querySelector(".container");
+const newGrid = document.querySelector(".new-grid");
+const showGrid = document.querySelector(".show-grid");
 
 container.style.width = `${WIDTH}px`;
 container.style.height = `${HEIGHT}px`;
 
 let isSketching = false;
+let cellNumber = 16;
 
 container.addEventListener("mousedown", () => isSketching = true);
 container.addEventListener("mouseup", () => isSketching = false);
 
-for (let i = 0; i < CELL_NUMBER * CELL_NUMBER; i++) {
-  const cell = document.createElement("div");
-  cell.style.width = `${WIDTH/CELL_NUMBER}px`;
-  cell.style.height = `${HEIGHT/CELL_NUMBER}px`;
-  cell.classList.add("cell");
-  container.appendChild(cell);
-  cell.addEventListener("mousemove", function() {
-	if (isSketching) {
-	cell.style["background-color"] = "black";
-	}
-  });
+newGrid.addEventListener("click", function() {
+  const newCellNumber = Number(prompt("Number of rows? (10-100)"));
+  if (isNaN(newCellNumber) || newCellNumber < 10 || newCellNumber > 100) {
+	alert("Number between 10 and 100 please!");
+	return;
+  } else {
+	cellNumber = newCellNumber;
+  }
+ 
+  clearBoard();
+   makeGrid(cellNumber);
+
+  function clearBoard() {
+	let cells = document.querySelectorAll(".cell");
+	console.log(cells);
+	cells.forEach(c => container.removeChild(c));
+	cells = null;
+	console.log(cells);
+  }
+}); 
+
+function makeGrid(cellNumber) {
+  for (let i = 0; i < cellNumber * cellNumber; i++) {
+	const cell = document.createElement("div");
+	cell.style.width = `${WIDTH/cellNumber}px`;
+	cell.style.height = `${HEIGHT/cellNumber}px`;
+	cell.classList.add("cell");
+	container.appendChild(cell);
+	cell.addEventListener("mousemove", function() {
+	  if (isSketching) {
+	  cell.style["background-color"] = "blue";
+	  }
+	});
+  }
+  showGrid.classList.remove("active");
 }
+
+showGrid.addEventListener("click", function() {
+  container.querySelectorAll(".cell").forEach(c => c.classList.toggle("grid"));
+  showGrid.classList.toggle("active");
+});
+
+makeGrid(cellNumber);
